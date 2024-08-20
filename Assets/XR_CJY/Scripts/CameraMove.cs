a1882954e75d0d96c843ba0ce9dae7aea687e117
Popcorn0212 <cwd8564@gmail.com>
2024-08-20 19:52:55 +0900

    3

--- Assets/XR_CJY/Scripts/CameraMove.cs
+++ Assets/XR_CJY/Scripts/CameraMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 2. 방향, 레이 생성, 체크 거리
+        // 2-1. 레이를 만든다.
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        // 2-2. 레이가 충돌한 대상의 정보를 담기 위한 구조체를 생성한다.
+        RaycastHit hitInfo;
+
+        // 2-3. 만들어진 레이를 지정된 방향과 거리만큼 발사한다.
+        bool isHit = Physics.Raycast(ray, out hitInfo, 3, ~(1 << 7));
+        print(hitInfo);
     }
 }
