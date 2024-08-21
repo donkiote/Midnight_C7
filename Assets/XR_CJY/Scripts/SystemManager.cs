bad3b6e0886c2919fcd4c656502a88e8a0de1741
Popcorn0212 <cwd8564@gmail.com>
2024-08-22 02:08:35 +0900

    ui

--- Assets/XR_CJY/Scripts/SystemManager.cs
+++ Assets/XR_CJY/Scripts/SystemManager.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using StarterAssets;
+using UnityEditor;
 using UnityEngine;
 
 public class SystemManager : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject img_Menu;
+    bool isOn = false;
 
-    FirstPersonController fpc;
-    Highlight hl;
-
-    void Start()
+    void Update()
     {
-        fpc = player.GetComponent<FirstPersonController>();
+        if (!isOn)
+        {
+            // Esc 키를 누를 시 게임 메뉴 화면 활성화
+            if (Input.GetButtonDown("Cancel"))
+            {
+                isOn = true;
+                Cursor.lockState = CursorLockMode.None;
+                img_Menu.SetActive(true);
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                isOn = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                img_Menu.SetActive(false);
+            }
+        }
     }
 
-    public void ExitButton()
+    public void Quit()
     {
-        hl.paintUI.gameObject.SetActive(false);
-        Cursor.lockState = CursorLockMode.Locked;
-        fpc.enabled = true;
+#if UNITY_EDITOR
+        // 1. 에디터일 경우 플레이 모드 끄기
+        EditorApplication.ExitPlaymode();
+#elif UNITY_STANDALONE
+        // 2. 빌드한 어플리케이션일 경우 앱 종료
+        Application.Quit();
+#endif
     }
 }
